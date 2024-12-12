21ffe0fa78b4c80c85b36fd24744823eff563a2e
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-12-12 15:23:58 +0300

    unittest -->CreateUserValidator_WhenPasswordIsTooShort_ShouldHaveValidationError

--- QuizAPI/Test/Application.Tests/Validators/CreateUserCommandRequestValidatorTests.cs
+++ QuizAPI/Test/Application.Tests/Validators/CreateUserCommandRequestValidatorTests.cs
@@ -40,90 +40,19 @@ public class CreateUserCommandRequestValidatorTests
         result.Errors.Should().Contain(x => x.PropertyName == "Email" && x.ErrorMessage == "Please enter a valid email address.");
     }
 
-    //[Fact]
-    //public async Task CreateUserValidator_WhenPasswordIsInvalid_ShouldHaveValidationError()
-    //{
-    //    // Arrange
-    //    var command = new CreateUserCommandRequest
-    //    {
-    //        Email = "test@example.com",
-    //        Password = "short",
-    //        UserName = "TestUser"
-    //    };
+    [Fact]
+    public async Task CreateUserValidator_WhenPasswordIsTooShort_ShouldHaveValidationError()
+    {
+        // Arrange
+        var request = new CreateUserCommandRequest("TestUser", "test@example.com", "short");
+      
+        // Act
+        var result = _validator.Validate(request);
 
-    //    // Act
-    //    var result = _validator.Validate(command);
-
-    //    // Assert
-    //    result.Errors.Should().Contain(x => x.PropertyName == "Password" && x.ErrorMessage == "Password length must be at least 6 characters");
-    //}
-
-    //[Fact]
-    //public async Task CreateUserValidator_WhenPasswordDoesNotContainUpperCase_ShouldHaveValidationError()
-    //{
-    //    // Arrange
-    //    var command = new CreateUserCommandRequest
-    //    {
-    //        Email = "test@example.com",
-    //        Password = "password1!",
-    //        UserName = "TestUser"
-    //    };
-
-    //    // Act
-    //    var result = _validator.Validate(command);
-
-    //    // Assert
-    //    result.Errors.Should().Contain(x => x.PropertyName == "Password" && x.ErrorMessage == "Password must contain at least one uppercase letter");
-    //}
-
-    //[Fact]
-    //public async Task CreateUserValidator_WhenPasswordDoesNotContainSpecialCharacter_ShouldHaveValidationError()
-    //{
-    //    // Arrange
-    //    var command = new CreateUserCommandRequest
-    //    {
-    //        Email = "test@example.com",
-    //        Password = "Password123",
-    //        UserName = "TestUser"
-    //    };
-
-    //    // Act
-    //    var result = _validator.Validate(command);
-
-    //    // Assert
-    //    result.Errors.Should().Contain(x => x.PropertyName == "Password" && x.ErrorMessage == "Password must contain at least one special character");
-    //}
-
-    //[Fact]
-    //public async Task CreateUserValidator_WhenUserNameIsTooShort_ShouldHaveValidationError()
-    //{
-    //    // Arrange
-    //    var command = new CreateUserCommandRequest
-    //    {
-    //        Email = "test@example.com",
-    //        Password = "Password1!",
-    //        UserName = "Usr"
-    //    };
-
-    //    // Act
-    //    var result = _validator.Validate(command);
-
-    //    // Assert
-    //    result.Errors.Should().Contain(x => x.PropertyName == "UserName" && x.ErrorMessage == "Username must be at least 4 characters long");
-    //}
-
-    //[Fact]
-    //public async Task CreateUserValidator_WhenAllFieldsAreValid_ShouldNotHaveValidationErrors()
-    //{
-    //    // Arrange
-    //    var command = new CreateUserCommandRequest("test@example.com", "Password1!", "TestUser");
+        // Assert
+        result.Errors.Should().Contain(x => x.PropertyName == "Password" && x.ErrorMessage == "Password length must be at least 6 characters");
+   
+    }
 
 
-
-    //    // Act
-    //    var result = _validator.Validate(command);
-
-    //    // Assert
-    //    result.Errors.Should().BeEmpty();
-    //}
 }
