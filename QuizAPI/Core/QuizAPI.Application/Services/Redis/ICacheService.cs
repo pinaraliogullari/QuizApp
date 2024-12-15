5d0556634e471095834de7069f8b6b54b45f5847
Pınar ALİOĞULLARI <aliogullarpnar@gmail.com>
2024-12-15 12:16:59 +0300

    ICacheService interfaces's methods have written.

--- QuizAPI/Core/QuizAPI.Application/Services/Redis/ICacheService.cs
+++ QuizAPI/Core/QuizAPI.Application/Services/Redis/ICacheService.cs
@@ -2,4 +2,7 @@
 
 public interface ICacheService
 {
+    Task SetAsync<T>(string key, T value, TimeSpan absoluteExpirationRelativeToNow, TimeSpan slidingExpiration = default);
+    Task<T> GetAsync<T>(string key);
+    Task RemoveAsync(string key);
 }
--- QuizAPI/Infrastructure/QuizAPI.Infrastructure/Services/Redis/CacheService.cs
+++ QuizAPI/Infrastructure/QuizAPI.Infrastructure/Services/Redis/CacheService.cs
@@ -1,5 +1,21 @@
-﻿namespace QuizAPI.Infrastructure.Services.Redis;
+﻿using QuizAPI.Application.Services.Redis;
 
-public class CacheService
+namespace QuizAPI.Infrastructure.Services.Redis;
+
+public class CacheService : ICacheService
 {
+    public Task<T> GetAsync<T>(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task RemoveAsync(string key)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task SetAsync<T>(string key, T value, TimeSpan absoluteExpirationRelativeToNow, TimeSpan slidingExpiration = default)
+    {
+        throw new NotImplementedException();
+    }
 }
